b76cf65941017d8d8e573afe93668aca78a09b7a
varia.selina <variagames.edu@gmail.com>
2023-03-10 20:41:08 +0300

    updates gcd-game

--- src/games/gcd-game.js
+++ src/games/gcd-game.js
@@ -5,7 +5,7 @@ export const rules = 'Find the greatest common divisor of given numbers.';
 const getGcd = (a, b) => {
 	let divisor;
 	for (let i = 1; i <= a && i <= b; i++) {
-		if (a % i == 0 && b % i == 0) {
+		if (a % i === 0 && b % i === 0) {
 			divisor = i;
 		}
 	}
@@ -14,8 +14,8 @@ const getGcd = (a, b) => {
 
 
 export const questionCorrectAnswer = () => {
-	const a = getRandomNum(0, 100);
-	const b = getRandomNum(0, 100);
+	const a = getRandomNum(1, 100);
+	const b = getRandomNum(1, 100);
 	const expr = `${a} ${b}`;
 	const correctAnswer = getGcd(a, b).toString();
 	return [expr, correctAnswer];
