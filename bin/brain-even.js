30fcec44670235ffe3e36d94ee33011df55a5904
varia.selina <variagames.edu@gmail.com>
2023-03-10 18:54:01 +0300

    update brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { greeting, getName, evenGame } from "../src/games/even-game";
+import { greeting, getName, evenGame } from "../src/games/even-game.js";
 
 greeting();
 const name = getName();
