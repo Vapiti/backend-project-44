96552e3797a6200ae2ad0867f598295edbb1812b
varia.selina <variagames.edu@gmail.com>
2023-03-10 22:06:56 +0300

    updates of prime

--- src/games/prime-game.js
+++ src/games/prime-game.js
@@ -5,7 +5,9 @@ export const rules = 'Answer "yes" if given number is prime. Otherwise answer "n
 export const checkPrime = (num) => {
   if (num === 1) {
     return false;
-  } else if (num === 2) {
+  }
+
+  if (num === 2) {
     return true;
   }
 
