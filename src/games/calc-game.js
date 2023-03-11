6798ccb0f06665e938b6a8491326c9145330ecf2
varia.selina <variagames.edu@gmail.com>
2023-03-11 20:49:03 +0300

    update of calc-game.js

--- src/games/calc-game.js
+++ src/games/calc-game.js
@@ -20,7 +20,7 @@ const calc = (a, b, operator) => {
 const questionCorrectAnswer = () => {
   const a = getRandomNum(0, 50);
   const b = getRandomNum(0, 50);
-  const operator = operators[getRandomNum(0, operators.length)];
+  const operator = operators[getRandomNum(0, operators.length - 1)];
   const question = `${a} ${operator} ${b}`;
   const correctAnswer = calc(a, b, operator).toString();
   return [question, correctAnswer];
