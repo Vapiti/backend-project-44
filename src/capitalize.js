3700b87d4549578517709f976cef8546ffd430b5
varia.selina <variagames.edu@gmail.com>
2023-06-12 18:05:57 +0300

    fresh updates in capitalizes

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,6 +1,6 @@
 const capitalize = (text) => {
-  const [firstSymbol = '', ...restSymbols] = text;
+  const [firstSymbol = '', restSymbols] = text;
   return `${firstSymbol.toUpperCase()}${restSymbols.join('')}`;
 };
 
-export default capitalize;
+export default capitalize();
